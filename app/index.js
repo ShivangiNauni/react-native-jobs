5b73de160a192562d7c908f831a05f9d622c8241
ShivangiNauni <shivanginauni01@gmail.com>
2024-02-02 15:43:47 +0530

    render one exercise

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import {View, Text, StyleSheet} from 'react-native';
+
+const Home = ()=>{
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
