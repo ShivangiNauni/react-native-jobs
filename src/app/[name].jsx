44b51dcbd070d48257a9893f70d1cb111bcd4b46
ShivangiNauni <shivanginauni01@gmail.com>
2024-02-05 00:51:08 +0530

    setup graphQL api using stepzen

--- src/app/[name].jsx
+++ src/app/[name].jsx
@@ -3,8 +3,11 @@ import { Link } from "expo-router";
 import { useLocalSearchParams } from "expo-router";
 import exercises from "../../assets/data/exercises.json";
 import { Stack } from "expo-router";
+import { useState } from "react";
 export default function ExerciseDetailsScreen() {
   const params = useLocalSearchParams();
+
+  const [isInstExpanded, setIsInstExpanded] = useState(false);
   const exercise = exercises.find((item) => item.name === params.name);
 
   if (!exercise) {
@@ -19,13 +22,24 @@ export default function ExerciseDetailsScreen() {
     <ScrollView contentContainerStyle={styles.container}>
       <Stack.Screen options={{ title: exercise.name }} />
       <View style={styles.panel}>
-      <Text style={styles.exerciseName}>{exercise.name}</Text>
-      <Text style={styles.exerciseSubtitle}>
-        {exercise.muscle.toUpperCase()} | {exercise.equipment.toUpperCase()}
-      </Text>
+        <Text style={styles.exerciseName}>{exercise.name}</Text>
+        <Text style={styles.exerciseSubtitle}>
+          {exercise.muscle.toUpperCase()} | {exercise.equipment.toUpperCase()}
+        </Text>
       </View>
       <View style={styles.panel}>
-      <Text style={styles.instructions}>{exercise.instructions}</Text>
+        <Text
+          numberOfLines={isInstExpanded ? 0 : 3}
+          style={styles.instructions}
+        >
+          {exercise.instructions}
+        </Text>
+        <Text
+          onPress={() => setIsInstExpanded(!isInstExpanded)}
+          style={styles.seeMore}
+        >
+          {isInstExpanded ? "See less" : "See more"}
+        </Text>
       </View>
     </ScrollView>
   );
@@ -34,19 +48,24 @@ export default function ExerciseDetailsScreen() {
 const styles = StyleSheet.create({
   container: {
     padding: 10,
-    gap:5
+    gap: 5,
   },
   exerciseName: { fontSize: 20, fontWeight: "500" },
   exerciseSubtitle: { color: "dimgray" },
 
   instructions: {
-    fontSize:16,
-    lineHeight:22,
-    
+    fontSize: 16,
+    lineHeight: 22,
   },
-  panel:{
-    backgroundColor:'white',
-padding:10,
-borderRadius:5
-  }
+  panel: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+  },
+  seeMore: {
+    alignSelf: "center",
+    padding: 10,
+    fontWeight: "600",
+    color: "gray",
+  },
 });
